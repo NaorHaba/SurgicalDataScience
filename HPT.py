f9dd771be16e55ffd58c3b9b1269d0361c899c68
LiorYariv1 <lior.ya1994@gmail.com>
2022-03-04 18:42:46 +0200

    Update HPT.py

--- HPT.py
+++ HPT.py
@@ -4,7 +4,7 @@ from train_experiment_optuna import main
 
 if __name__ == '__main__':
     # HPT_Tune_1
-    study = optuna.create_study(study_name = 'best_cur_arch_1', direction='maximize')
+    study = optuna.create_study(study_name = 'HPT_tune_4', direction='maximize')
     # study = optuna.create_study(study_name = 'debug', direction='maximize')
 
     study.optimize(main)
